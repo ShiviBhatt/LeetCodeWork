e56ad43c31f6e901fc566e4f25d04f1ee7354e0f
ShiviBhatt <in.shivi@gmail.com>
2020-08-28 15:50:28 -0700

    Leet Code Problem Solved

---
+++ src/easy/Q412FizzBuzz.java
@@ -0,0 +1,23 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q412FizzBuzz {
+	public List<String> fizzBuzz(int n) {
+		List<String> result = new ArrayList<String>();
+		for (int i = 1; i <= n; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				result.add("FizzBuzz");
+			} else if (i % 3 == 0) {
+				result.add("Fizz");
+			} else if (i % 5 == 0) {
+				result.add("Buzz");
+			} else {
+				result.add(Integer.toString(i));
+			}
+		}
+		return result;
+	}
+
+}
